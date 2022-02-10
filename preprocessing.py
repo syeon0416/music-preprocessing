3f9aece0f9b4d285bec6d7c7f07442d3253115f2
syeon0416 <syeon0416@moe.work>
2022-02-10 11:21:10 +0900

    bug fix

--- preprocessing.py
+++ preprocessing.py
@@ -10,12 +10,12 @@ x, sr = librosa.load('audio.wav')
 
 waveform = librosa.load(path, sample_rate)[0]
 melspec = librosa.feature.melspectrogram(waveform, sr=sample_rate, n_mels=128)
-log_melspec = librosa.power_to_db(melspec, reg=np.max)
+log_melspec = librosa.power_to_db(melspec, ref=np.max)
 mfcc = librosa.feature.mfcc(S=log_melspec, n_fmcc=20)
 
 delta2_mfcc = librosa.feature.delta(mfcc, order=2)
 
-plt.fugure(figsize=(12, 4))
+plt.figure(figsize=(12, 4))
 librosa.display.specshow(delta2_mfcc)
 plt.ylabel('MFCC coeffs')
 plt.xlabel('Time')
